06be1620d077d6bb62de2d8fba45c0496b8f1206
Faiz Ahmed <4faizahmed@gmail.com>
2020-10-24 17:17:05 +0300

    Autocomplete search structure set up

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-console.log('Hi there!');
+const fetchData = async function(searchTerm){
+  const response = await axios.get("http://www.omdbapi.com", {
+      params: {
+          apikey: '33b6e284',
+          s: searchTerm
+      }
+  });
+
+    console.log(response.data);
+};
+
+
+
+const input = document.querySelector("input");
+
+const debounce = (func) => {
+    let timeoutId;
+    return (...args) => {
+        if (timeoutId){
+            clearTimeout(timeoutId);
+        }
+        timeoutId = setTimeout(() => {
+            func.apply(null, args);
+        }, 500);
+    }
+};
+
+
+const onInput = debounce(event => {
+        fetchData(event.target.value);
+});
+
+
+input.addEventListener("input", onInput);
