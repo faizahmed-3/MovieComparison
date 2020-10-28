b4454f89ae00364af747f823f0e448e8a0d0d815
Faiz Ahmed <4faizahmed@gmail.com>
2020-10-28 17:24:50 +0300

    Search menu basic functionality complete

--- index.js
+++ index.js
@@ -1,34 +1,64 @@
-const fetchData = async function(searchTerm){
-  const response = await axios.get("http://www.omdbapi.com", {
-      params: {
-          apikey: '33b6e284',
-          s: searchTerm
-      }
-  });
+const fetchData = async function (searchTerm) {
+    const response = await axios.get("http://www.omdbapi.com", {
+        params: {
+            apikey: '33b6e284',
+            s: searchTerm
+        }
+    });
 
-    console.log(response.data);
+    if (response.data.Error) {
+        return [];
+    }
+
+    return response.data.Search;
 };
 
 
+const root = document.querySelector('.autocomplete');
+
+root.innerHTML = `
+    <label><b>Search For a Movie</b></label>
+    <input class="input">
+    <div class="dropdown">
+        <div class="dropdown-menu">
+            <div class="dropdown-content results"></div>
+        </div>
+    </div>
+`;
 
 const input = document.querySelector("input");
+const dropdown = document.querySelector(".dropdown");
+const resultsWrapper = document.querySelector(".results");
 
-const debounce = (func) => {
-    let timeoutId;
-    return (...args) => {
-        if (timeoutId){
-            clearTimeout(timeoutId);
-        }
-        timeoutId = setTimeout(() => {
-            func.apply(null, args);
-        }, 500);
+
+const onInput = async event => {
+    const movies = await fetchData(event.target.value);
+
+    resultsWrapper.innerHTML = '';
+
+    dropdown.classList.add('is-active');
+    for (let movie of movies) {
+        const option = document.createElement('a');
+        const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
+
+        option.classList.add('dropdown-item');
+        option.innerHTML = `
+            <img src='${imgSrc}'/>
+            ${movie.Title}
+        `;
+        resultsWrapper.append(option);
     }
+
 };
 
 
-const onInput = debounce(event => {
-        fetchData(event.target.value);
-});
+input.addEventListener("input", debounce(onInput, 500));
 
 
-input.addEventListener("input", onInput);
+
+
+document.addEventListener('click', event => {
+    if (!root.contains(event.target)) {
+        dropdown.classList.remove('is-active');
+    }
+})
